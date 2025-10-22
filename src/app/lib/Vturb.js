ea33c536acbf967626a264114931dca8c7364347
Tapas <tapas.paul@codeclouds.in>
2025-10-22 11:29:40 +0530

    Implement Vturv component

---
+++ src/app/lib/Vturb.js
@@ -0,0 +1,8 @@
+const Vturb = () => {
+    return(
+        <div className="relative aspect-video">
+            <iframe className="w-full h-full absolute top-0 right-0 bottom-0 left-0 object-cover object-center"></iframe>
+        </div>
+    )
+}
+export default Vturb;
