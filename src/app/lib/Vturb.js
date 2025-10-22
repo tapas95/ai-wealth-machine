e1b03246585841b4d2eb0b2634640bae26648b25
Tapas <tapas.paul@codeclouds.in>
2025-10-22 21:43:40 +0530

    Implemented video using Vturb

--- src/app/lib/Vturb.js
+++ src/app/lib/Vturb.js
@@ -1,8 +1,20 @@
+'use client';
+import React, { useEffect } from "react";
+
 const Vturb = () => {
+    const videoHTML = `<vturb-smartplayer id="vid-68f0c63981e93bf7cd48dc02" data-autoplay="1" 
+        data-muted="1"></vturb-smartplayer>`;
+    useEffect( () => {
+        const s = document.createElement( "script" );
+        s.src="https://scripts.converteai.net/2ff821eb-61c4-4c7f-9ce4-865740eaf07e/players/68f0c63981e93bf7cd48dc02/v4/player.js";
+        s.async=!0;
+        document.head.appendChild( s );
+        return () => {
+            document.head.removeChild( s );
+        }
+    }, [] );
     return(
-        <div className="relative aspect-video">
-            <iframe className="w-full h-full absolute top-0 right-0 bottom-0 left-0 object-cover object-center"></iframe>
-        </div>
+        <div className="sm:border-2 border-white sm:rounded-3xl overflow-hidden sm:shadow-primary" dangerouslySetInnerHTML={ { __html: videoHTML } } />
     )
 }
 export default Vturb;
--- src/app/pages/Landing.js
+++ src/app/pages/Landing.js
@@ -1,21 +1,21 @@
 import Image from "next/image";
 import Button from "@/app/components/ui/Button";
+import Vturb from "@/app/lib/Vturb";
 import landingBg from '@/app/assets/images/landing-bg.png';
-import videoPlaceholder from '@/app/assets/images/video-placeholder.png';
 import { PiEyeFill } from "react-icons/pi";
 
 const Landing = () => {
     return(
         <section className="bg-cover bg-center bg-no-repeat" style={ { backgroundImage: `url('${ landingBg.src }')` } }>
           <div className="container sm:px-4 mx-auto">
-            <div className="flex items-center justify-center py-8 max-w-5xl mx-auto" style={ { minHeight: 'calc(100vh - 176px)' } }>
-              <div className="relative">
-                <Image src={ videoPlaceholder } alt="Landing background" className="block max-w-full h-auto" />
-                <Button className="items-center gap-2 absolute left-1/2 bottom-[3%] -translate-x-1/2">
-                  <PiEyeFill size={ 16 } className="block" />
-                  <span className="block">812 Are Watching The Video</span>
-                </Button>
-              </div>
+            <div className="flex items-center justify-center py-8 w-[1100px] max-w-full mx-auto" style={ { minHeight: 'calc(100vh - 176px)' } }>
+                <div className="relative w-full">
+                  <Vturb />
+                  <Button className="items-center gap-2 absolute left-1/2 bottom-0 -translate-x-1/2 translate-y-1/2 z-10">
+                    <PiEyeFill size={ 16 } className="block" />
+                    <span className="block">812 Are Watching The Video</span>
+                  </Button>
+                </div>
             </div>
           </div>
         </section>
